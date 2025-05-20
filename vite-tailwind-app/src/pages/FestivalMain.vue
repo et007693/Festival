991da3926ddd90a2ebf85d9e71a0e8a67e026c70
송창용 <thdckdyd123@naver.com>
2025-05-20 15:27:23 +0900

    header 추가

---
+++ vite-tailwind-app/src/pages/FestivalMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+      <div class="flex w-full items-center m-3 gap-3">
+        <img class="w-14" src="../assets/pngwing.com.png" alt="" />
+        <p class="text-4xl text-orange-800">Festival</p>
+      </div>
+    </nav>
+
+    <main>
+      <div class="flex justify-center align h-screen items-center">
+        <h1>달력</h1>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
