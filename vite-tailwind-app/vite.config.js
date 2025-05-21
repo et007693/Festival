e412c9df65f5fca2647329ea0cab1af1a1864fd3
ksm <tpals555@daum.net>
2025-05-21 09:40:10 +0900

    ksm merge

--- vite-tailwind-app/vite.config.js
+++ vite-tailwind-app/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  optimizeDeps: {
+    include: [
+      "@fullcalendar/core",
+      "@fullcalendar/daygrid",
+      "@fullcalendar/timegrid",
+      "@fullcalendar/interaction",
+      "@fullcalendar/vue3",
+    ],
+  },
+});
